db4a42b01147f0724a7621c455f6693a05d00dcc
liwentao <liwentao0705@outlook.com>
2020-07-25 22:23:06 +0800

    微安表模板

--- Assets/Scripts/Entity/SampleuA.cs
+++ Assets/Scripts/Entity/SampleuA.cs
@@ -1,14 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using SpiceSharp.Components;
 
 /// <summary>
 /// 微安表，有各种量程和内阻的规格，内阻为标称
 /// </summary>
-public class SampleuA : MonoBehaviour
+public class SampleuA : EntityBase, IAmmeter
 {
-    // Start is called before the first frame update
-    void Start()
+    public double MaxI0 = 0.05;
+	public double R0 = 2;
+
+	public override void EntityAwake()
+	{
+		//
+	}
+
+	public override void LoadElement()
+	{
+		int GND = ChildPorts[0].ID;
+		int V0 = ChildPorts[1].ID;
+		CircuitCalculator.UF.Union(GND, V0);
+	}
+
+	public override void SetElement()
+	{
+		int EntityID = CircuitCalculator.EntityNum;
+		int GND = ChildPorts[0].ID;
+		int V0 = ChildPorts[1].ID;
+		CircuitCalculator.SpiceEntities.Add(new Resistor(EntityID.ToString(), GND.ToString(), V0.ToString(), R0));
+	}
+
+	public override EntityData Save()
+	{
+		// 微安表属于简单元件
+		return new SimpleEntityData<SampleuA>(transform.position, transform.rotation, ChildPortID);
+	}
+
+	public void CalculateCurrent()
+	{
+		ChildPorts[1].I = (ChildPorts[1].U - ChildPorts[0].U) / R0;
+	}
+
+	// Start is called before the first frame update
+	void Start()
     {
         
     }
