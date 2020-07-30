f6b736500711e3f287807535b39e280e7e0871f2
liwentao0705 <liwentao0705@outlook.com>
2020-07-30 21:42:46 +0800

    待测电阻为1M时直接显示1M

--- Assets/Scripts/Entity/NominalR.cs
+++ Assets/Scripts/Entity/NominalR.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -6,42 +7,53 @@ using UnityEngine;
 /// </summary>
 public class NominalR : Resistance
 {
-	public bool RealValueSet = false;//置为1时，在生成时不随机变化
-	public double NominalValue;
+    public bool RealValueSet = false;//置为1时，在生成时不随机变化
+    public double NominalValue;
 
-	void Start()
-	{
-		if(!RealValueSet)
-		{
-			Value = Nominal.GetRealValue(NominalValue);
-			RealValueSet = true;
-		}
-		if (resistanceText) resistanceText.text = "待测\n" + NominalValue.ToString() + "Ω";
-	}
+    void Start()
+    {
+        if (!RealValueSet)
+        {
+            Value = Nominal.GetRealValue(NominalValue);
+            RealValueSet = true;
+        }
+        if (resistanceText)
+        {
+            if (Math.Abs(NominalValue - 1e6) < 0.001)
+            {
+                resistanceText.text = "待测\n1MΩ";
 
-	public override EntityData Save()
-	{
-		return new NominalRData(NominalValue, Value, transform.position, transform.rotation, ChildPortID);
-	}
+            }
+            else
+            {
+                resistanceText.text = "待测\n" + NominalValue.ToString() + "Ω";
+            }
+        }
+    }
+
+    public override EntityData Save()
+    {
+        return new NominalRData(NominalValue, Value, transform.position, transform.rotation, ChildPortID);
+    }
 }
 
 [System.Serializable]
 public class NominalRData : ResistanceData
 {
-	private readonly double nominalValue;
-	private readonly double realValue;
-	public NominalRData(double nominalValue, double realValue, Vector3 pos, Quaternion angle, List<int> id) : base(realValue, pos, angle, id)
-	{
-		this.nominalValue = nominalValue;
-		this.realValue = realValue;
-	}
+    private readonly double nominalValue;
+    private readonly double realValue;
+    public NominalRData(double nominalValue, double realValue, Vector3 pos, Quaternion angle, List<int> id) : base(realValue, pos, angle, id)
+    {
+        this.nominalValue = nominalValue;
+        this.realValue = realValue;
+    }
 
-	override public void Load()
-	{
-		NominalR resistance = EntityCreator.CreateEntity<NominalR>(posfloat, anglefloat, IDList);
-		// 读档要沿用原本生成的真实值
-		resistance.RealValueSet = true;
-		resistance.NominalValue = nominalValue;
-		resistance.Value = realValue;
-	}
+    override public void Load()
+    {
+        NominalR resistance = EntityCreator.CreateEntity<NominalR>(posfloat, anglefloat, IDList);
+        // 读档要沿用原本生成的真实值
+        resistance.RealValueSet = true;
+        resistance.NominalValue = nominalValue;
+        resistance.Value = realValue;
+    }
 }
