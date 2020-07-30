ac302aa186f4d1144ba2101c81ac9fc6e14a7d0f
liwentao0705 <liwentao0705@outlook.com>
2020-07-30 21:47:22 +0800

    精确电阻也加单位显示

--- Assets/Scripts/Entity/Resistance.cs
+++ Assets/Scripts/Entity/Resistance.cs
@@ -5,61 +5,75 @@ using UnityEngine.UI;
 
 public class Resistance : EntityBase
 {
-	public double Value = 120;
-	public Text resistanceText;
+    public double Value = 120;
+    public Text resistanceText;
 
-	public override void EntityAwake()
-	{
-		resistanceText = transform.FindComponent_DFS<Text>("Text");
-	}
+    public override void EntityAwake()
+    {
+        resistanceText = transform.FindComponent_DFS<Text>("Text");
+    }
 
-	const int maxSize = 7;//填满一行的字符数量
-	void Start()
-	{
-		// 不能在Awake(）中执行，Awake()之后还可能修改阻值
-		string str = Value.ToString() + "Ω";
-		if (str.Length > maxSize)
-		{
-			resistanceText.fontSize = resistanceText.fontSize * 7 / str.Length;
-		}
-		if (resistanceText) resistanceText.text = str;
-	}
+    const int maxSize = 7;//填满一行的字符数量
+    void Start()
+    {
+        // 不能在Awake(）中执行，Awake()之后还可能修改阻值
+        string str;
 
-	public override void LoadElement()
-	{
-		int LeftPortID = ChildPorts[0].ID;
-		int RightPortID = ChildPorts[1].ID;
+        if (Value > 1000000)
+        {
+            str = (Value / 1000000).ToString() + "MΩ";
+        }
+        else if (Value > 1000)
+        {
+            str = (Value / 1000).ToString() + "kΩ";
+        }
+        else
+        {
+            str = Value.ToString() + "Ω";
+        }
 
-		CircuitCalculator.UF.Union(LeftPortID, RightPortID);
-	}
+        if (str.Length > maxSize)
+        {
+            resistanceText.fontSize = resistanceText.fontSize * 7 / str.Length;
+        }
+        if (resistanceText) resistanceText.text = str;
+    }
 
-	public override void SetElement()
-	{
-		int EntityID = CircuitCalculator.EntityNum;
-		int LeftPortID = ChildPorts[0].ID;
-		int RightPortID = ChildPorts[1].ID;
+    public override void LoadElement()
+    {
+        int LeftPortID = ChildPorts[0].ID;
+        int RightPortID = ChildPorts[1].ID;
 
-		CircuitCalculator.SpiceEntities.Add(new Resistor(EntityID.ToString(), LeftPortID.ToString(), RightPortID.ToString(), Value));
-	}
+        CircuitCalculator.UF.Union(LeftPortID, RightPortID);
+    }
 
-	public override EntityData Save()
-	{
-		return new ResistanceData(Value, transform.position, transform.rotation, ChildPortID);
-	}
+    public override void SetElement()
+    {
+        int EntityID = CircuitCalculator.EntityNum;
+        int LeftPortID = ChildPorts[0].ID;
+        int RightPortID = ChildPorts[1].ID;
+
+        CircuitCalculator.SpiceEntities.Add(new Resistor(EntityID.ToString(), LeftPortID.ToString(), RightPortID.ToString(), Value));
+    }
+
+    public override EntityData Save()
+    {
+        return new ResistanceData(Value, transform.position, transform.rotation, ChildPortID);
+    }
 }
 
 [System.Serializable]
 public class ResistanceData : EntityData
 {
-	private readonly double value;
-	public ResistanceData(double value, Vector3 pos, Quaternion angle, List<int> IDList) : base(pos, angle, IDList)
-	{
-		this.value = value;
-	}
+    private readonly double value;
+    public ResistanceData(double value, Vector3 pos, Quaternion angle, List<int> IDList) : base(pos, angle, IDList)
+    {
+        this.value = value;
+    }
 
-	override public void Load()
-	{
-		Resistance resistance = EntityCreator.CreateEntity<Resistance>(posfloat, anglefloat, IDList);
-		resistance.Value = value;
-	}
+    override public void Load()
+    {
+        Resistance resistance = EntityCreator.CreateEntity<Resistance>(posfloat, anglefloat, IDList);
+        resistance.Value = value;
+    }
 }
