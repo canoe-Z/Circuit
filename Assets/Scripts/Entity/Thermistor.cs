b896adcee1b4b3f94611ae45efe7b65f2b4093ef
liwentao0705 <liwentao0705@outlook.com>
2020-08-13 01:48:58 +0800

    几个新元件的模板

---
+++ Assets/Scripts/Entity/Thermistor.cs
@@ -0,0 +1,34 @@
+﻿using SpiceSharp.Components;
+
+/// <summary>
+/// TODO:热敏电阻
+/// </summary>
+public class Thermistor : EntityBase
+{
+	public override void EntityAwake()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	void Start()
+	{
+		throw new System.NotImplementedException();
+	}
+
+
+	public override void LoadElement()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override void SetElement(int entityID)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override EntityData Save()
+	{
+		throw new System.NotImplementedException();
+	}
+}
+
---
+++ Assets/Scripts/Entity/Triode.cs
@@ -0,0 +1,33 @@
+﻿using SpiceSharp.Components;
+
+/// <summary>
+/// TODO:晶体管
+/// </summary>
+public class Triode : EntityBase
+{
+	public override void EntityAwake()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	void Start()
+	{
+		throw new System.NotImplementedException();
+	}
+
+
+	public override void LoadElement()
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override void SetElement(int entityID)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	public override EntityData Save()
+	{
+		throw new System.NotImplementedException();
+	}
+}
