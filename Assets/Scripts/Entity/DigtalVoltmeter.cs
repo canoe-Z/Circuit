c140f2f8f7e4db360cf54231b60519144acef374
liwentao0705 <liwentao0705@outlook.com>
2020-08-22 00:13:25 +0800

    为数字电压表添加开关

--- Assets/Scripts/Entity/DigtalVoltmeter.cs
+++ Assets/Scripts/Entity/DigtalVoltmeter.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 public class DigtalVoltmeter : EntityBase, ICalculatorUpdate
 {
 	private readonly double R = 15000;
-	private MySwitch mySwitch;
-	private Text digtalDigtalVoltmeter;
 	private int PortID_GND, PortID_mV, PortID_V;
-	private bool isOnTest = false;
+
+	private Text digtalDigtalVoltmeter;
+	private MySwitch mySwitch;
 
 	public override void EntityAwake()
 	{
 		digtalDigtalVoltmeter = transform.FindComponent_DFS<Text>("Text");
+		mySwitch = transform.FindComponent_DFS<MySwitch>("MySwitch");
+
+		// 默认启动时开机，读档可覆盖该设置
+		mySwitch.IsOn = true;
 	}
 
 	void Start()
@@ -44,7 +48,7 @@ public class DigtalVoltmeter : EntityBase, ICalculatorUpdate
 			digtalDigtalVoltmeter.text = EntityText.GetText(0, 2);
 		}
 
-		if (!isOnTest)
+		if (!mySwitch.IsOn)
 		{
 			digtalDigtalVoltmeter.text = EntityText.GetText(0, 2);
 		}
@@ -54,23 +58,12 @@ public class DigtalVoltmeter : EntityBase, ICalculatorUpdate
 
 	public override void SetElement(int entityID)
 	{
-		//TODO:开关
-		if (isOnTest)
+		if (mySwitch.IsOn)
 		{
-			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_mV"), PortID_GND.ToString(), PortID_mV.ToString(), R));
-			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_V"), PortID_GND.ToString(), PortID_V.ToString(), R));
+			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_Off_mV"), PortID_GND.ToString(), PortID_mV.ToString(), R));
+			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_Off_V"), PortID_GND.ToString(), PortID_V.ToString(), R));
 
 			CircuitCalculator.SpicePorts.AddRange(ChildPorts);
-		}
-		else
-		{
-			Debug.LogWarning("开关关闭");
-			/*
-			CircuitCalculator.SpiceEntities.Add(new CurrentSource(string.Concat(entityID, "_Off_mV"), PortID_GND.ToString(), PortID_mV.ToString(), 0));
-			CircuitCalculator.SpiceEntities.Add(new CurrentSource(string.Concat(entityID, "_Off_V"), PortID_GND.ToString(), PortID_V.ToString(), 0));
-			*/
-			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_Off_mV"), PortID_GND.ToString(), PortID_mV.ToString(), 1e12));
-			CircuitCalculator.SpiceEntities.Add(new Resistor(string.Concat(entityID, "_Off_V"), PortID_GND.ToString(), PortID_V.ToString(), 1e12));
 		}
 	}
 
