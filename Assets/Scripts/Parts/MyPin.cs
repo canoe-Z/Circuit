e5844b1c84ce6640a36d2db002cb98a55011bb3f
liwentao <liwentao0705@outlook.com>
2020-09-03 16:01:13 +0800

    [实验性}模拟指针表电磁阻尼

--- Assets/Scripts/Parts/MyPin.cs
+++ Assets/Scripts/Parts/MyPin.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 /// </summary>
 public class MyPin : MonoBehaviour
 {
-	private float pos = 0;                              //当前的位置，0-1
+	public float pos = 0;                               //当前的位置，0-1
 	private string unitSymbol = "uA";                   //显示在表盘的字符串
 	private int maxScale = 100;                         //最大刻度
 	private Transform thePin;                           //指针（物理意义）
@@ -56,6 +56,8 @@ public class MyPin : MonoBehaviour
 		// 指针转动
 		thePin.transform.localEulerAngles = new Vector3(0, 0, 50 - 100 * pos);
 	}
+
+	public void SetPos(double newPos) => SetPos((float)newPos);
 
 	/// <summary>
 	/// 设置字符串
