b552554dacd2a74f87c90593dd27273a2f0cbc79
liwentao <liwentao0705@outlook.com>
2020-09-03 20:42:04 +0800

    Unity 2020

---
+++ Assets/PNG/Materials/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private bool isEmission = false;
+    // Start is called before the first frame update
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(!isEmission)
+			{
+                GetComponent<Renderer>().material.SetColor("Color_592D9D79", Color.yellow);
+                isEmission = true;
+            }
+            else
+			{
+                GetComponent<Renderer>().material.SetColor("Color_592D9D79", Color.black);
+                isEmission = false;
+            }
+        }
+    }
+}
