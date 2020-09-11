795c831d5a65ac5786db061bb78380ca046a9ce9
chaos <1938679111@qq.com>
2020-09-11 20:08:08 +0800

    导入一堆奇怪的东西

--- Assets/Scripts/Menu/WdwMenu_Save.cs
+++ Assets/Scripts/Menu/WdwMenu_Save.cs
@@ -5,54 +5,35 @@ using UnityEngine.UI;
 
 public class WdwMenu_Save : MonoBehaviour
 {
-	public Button btnLast;
-	public Button btnNext;
 	public Button btnSave;
 	public Button btnLoad;
-	public Image imgPic;
+
+	public Button save_0;
+	public Button save_1;
+	public Button save_2;
 	public Text txtIndex;
 
-	int nowIndex = 0;
-	int totalNum = 10;//总的存档数量
+	bool isLoading = true;
 	void Start()
 	{
-		btnLast.onClick.AddListener(ToLast);
-		btnNext.onClick.AddListener(ToNext);
 		btnSave.onClick.AddListener(Save);
 		btnLoad.onClick.AddListener(Load);
-		ChangeText();
+
+		Load();
 	}
 	//更新当前文本
 	void ChangeText()
 	{
-		txtIndex.text = "第" + (nowIndex + 1).ToString() + "/" + totalNum.ToString() + "档";
 	}
 
-	//下面是按钮
-	void ToLast()
-	{
-		nowIndex--;
-		if (nowIndex < 0)
-		{
-			nowIndex = totalNum - 1;
-		}
-		ChangeText();
-	}
-	void ToNext()
-	{
-		nowIndex++;
-		if (nowIndex >= totalNum)
-		{
-			nowIndex = 0;
-		}
-		ChangeText();
-	}
 	void Save()
 	{
-
+		isLoading = false;
+		txtIndex.text = "请选择保存的存档";
 	}
 	void Load()
 	{
-
+		isLoading = true;
+		txtIndex.text = "请选择加载的存档";
 	}
 }
