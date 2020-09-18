199be284578b0f19aa24697f5200728a02c79ade
chaos <1938679111@qq.com>
2020-09-19 00:06:48 +0800

    push

--- Assets/Scripts/Menu/WdwMenu_Save.cs
+++ Assets/Scripts/Menu/WdwMenu_Save.cs
@@ -10,44 +10,53 @@ public class WdwMenu_Save : MonoBehaviour
 	public InputField iptName;
 	public Canvas saveOrLoad;
 
-	public Button[] btnSaves;
+	public Button[] btnSavesAndPics;
 	Text[] txtSaves;
 
-	bool isLoading = true;
 	void Start()
 	{
+		//自己的画布
+		canvas = GetComponent<Canvas>();
+
 		btnSave.onClick.AddListener(Save);
 		btnLoad.onClick.AddListener(Load);
 
-		txtSaves = new Text[btnSaves.Length];
-		for (int i = 0; i < btnSaves.Length; i++)
+		txtSaves = new Text[btnSavesAndPics.Length];
+		for (int i = 0; i < btnSavesAndPics.Length; i++)
 		{
-			btnSaves[i].onClick.AddListener(delegate () { OnButtonSelect(i); });//添加带参数的按钮
-			txtSaves[i] = btnSaves[i].GetComponentInChildren<Text>();
+			btnSavesAndPics[i].onClick.AddListener(delegate () { OnButtonSelect(i); });//添加带参数的按钮
+
+			//处理按钮下面的文本
+			txtSaves[i] = btnSavesAndPics[i].GetComponentInChildren<Text>();
 			txtSaves[i].text = "hello\nworld";
 		}
 
 		saveOrLoad.enabled = false;
 	}
+	Canvas canvas;
+	public void SetCanvas(bool value)
+	{
+		canvas.enabled = value;
+		saveOrLoad.enabled = false;//每次切换都会关闭这个弹窗
+	}
 
+	int selectedID = 0;
 	void Save()
 	{
-		isLoading = false;
+		if (iptName.text == "") iptName.text = "default";
+		SaveManager.Instance.Save(selectedID, iptName.text);
+		saveOrLoad.enabled = true;//关闭弹窗
 	}
 	void Load()
 	{
-		isLoading = true;
+		SaveManager.Instance.Load(selectedID);
+		saveOrLoad.enabled = true;//关闭弹窗
 	}
 	void OnButtonSelect(int id)
 	{
-		saveOrLoad.enabled = true;
-		if (isLoading)
-		{
-			SaveManager.Instance.Load(id);
-		}
-		else
-		{
-			SaveManager.Instance.Save(id, "nmsl");
-		}
+		Debug.Log(id);
+		selectedID = id;
+		saveOrLoad.enabled = true;//打开弹窗
+		iptName.text = "";//清空弹窗的内容
 	}
 }
