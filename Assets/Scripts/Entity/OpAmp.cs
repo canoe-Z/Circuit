129dd7bd23e6ddf3bdc1104a4628540fca26116a
chaos <1938679111@qq.com>
2020-10-12 15:04:27 +0800

    push

--- Assets/Scripts/Entity/OpAmp.cs
+++ Assets/Scripts/Entity/OpAmp.cs
@@ -1,5 +1,8 @@
 ﻿using SpiceSharp.Components;
+using SpiceSharp.Entities;
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -34,19 +37,50 @@ public class OpAmp : EntityBase
 		CircuitCalculator.UF.ListUnion(new List<int> { PortID_N, PortID_P, PortID_O });
 	}
 
+
+	// 构建二极管模型
+	protected void ApplyParameters(Entity entity, string definition)
+	{
+		// Get all assignments
+		definition = Regex.Replace(definition, @"\s*\=\s*", "=");
+		var assignments = definition.Split(new[] { ',', ';', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+		foreach (var assignment in assignments)
+		{
+			// Get the name and value
+			var parts = assignment.Split('=');
+			if (parts.Length != 2)
+				throw new System.Exception("Invalid assignment");
+			var name = parts[0].ToLower();
+			var value = double.Parse(parts[1], System.Globalization.CultureInfo.InvariantCulture);
+
+			// Set the entity parameter
+			entity.SetParameter(name, value);
+		}
+	}
+	private DiodeModel CreateDiodeModel(string name, string parameters)
+	{
+		var dm = new DiodeModel(name);
+		ApplyParameters(dm, parameters);
+		return dm;
+	}
+
+
 	public override void SetElement(int entityID)
 	{
 		// 伪装Vcc和Vee的连接，读取其电压作为上下限
 
-		if(!NeedReset)
-        {
-			// 运放
-        }
-		else
-        {
-			// 输出上下限电压
-        }
-		throw new System.NotImplementedException();
+		PortID_G = ChildPorts[0].ID;
+		PortID_V = ChildPorts[1].ID;
+
+		CircuitCalculator.SpiceEntities.AddRange(new List<Entity>
+		{
+			CreateDiodeModel("Solar_1N4007", "Is=1.09774e-8 Rs=0.0414388 N=1.78309 Cjo=2.8173e-11 M=0.318974 tt=9.85376e-6 Kf=0 Af=1"),
+			new CurrentSource(string.Concat(entityID, "_S"), "S+", PortID_G.ToString(), Isc),
+			new Diode(string.Concat(entityID, "_D"), PortID_G.ToString(), "S+", "Solar_1N4007"),
+			new Resistor(string.Concat(entityID, "_R1"), "S+", PortID_G.ToString(), 10000),
+			new Resistor(string.Concat(entityID, "_R2"), PortID_V.ToString(), "S+", 0.5)
+		});
+
 	}
 
 	public void SaveLimit()
